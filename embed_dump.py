4af0b78497a7c64783adef06ea14ffe056122556
axhero7 <axhero7@gmail.com>
2024-05-25 17:17:53 -0700

    Update embed_dump.py

--- embed_dump.py
+++ embed_dump.py
@@ -10,13 +10,12 @@ def load_embed(epub_extension, modelname="mistral"):
     print("epubs: ", len(epub_extension))
     for filename in epub_extension:
         if not os.path.exists(f"embeddings/{filename}.json"):
-            with open(f"embeddings/{filename}.json", "r") as f:
-                chunks = parse_book(filename)
-                print("chunks: " + len(chunks))
-                emeddings= [ ollama.embeddings(model=modelname, prompt=chunk)["embedding"] for chunk in chunks]
-                print("embeddings: ", len(embeddings))
-                with open(f"embeddings/{filename}.json", "w") as f:
-                    json.dump(embeddings, f)
+            chunks = parse_book(filename)
+            print("chunks: " + len(chunks))
+            emeddings= [ ollama.embeddings(model=modelname, prompt=chunk)["embedding"] for chunk in chunks]
+            print("embeddings: ", len(embeddings))
+            with open(f"embeddings/{filename}.json", "w") as f:
+                json.dump(embeddings, f)
 def main():
     load_embed(get_epub('.'))
 
